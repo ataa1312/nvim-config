b25b7aa5ea0d74ec4192a61495d9f01dc106bc9e
Ata Shaker <atashaker13@gmail.com>
2025-04-15 17:24:24 +0200

    NVIM: Reconfigure TABS to be 4 Spaces

--- init.lua
+++ init.lua
@@ -3,3 +3,9 @@ print("Hello from NVIM")
 
 vim.cmd[[colorscheme tokyonight]]
 vim.o.relativenumber = true
+
+-- NOTE: TAB options
+vim.o.expandtab = true  -- NOTE: Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.tabstop = 4       -- NOTE: A TAB character looks like 4 spaces
+vim.o.softtabstop = 4   -- NOTE: Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4    -- NOTE: Number of spaces inserted when indenting
