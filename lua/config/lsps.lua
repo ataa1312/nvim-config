dcbb525e2dae3c619994b3f2a2ca5b69172defa6
Ata Shaker <atashaker13@gmail.com>
2025-04-27 23:44:58 +0200

    NVIM: Add plugin: nvim-lspconfig & Create lsps.lua

---
+++ lua/config/lsps.lua
@@ -0,0 +1,15 @@
+-- NOTE: Python LSP
+vim.lsp.enable("pyright")
+-- vim.lsp.config("pyright")
+
+-- NOTE: C/CPP LSP
+vim.lsp.enable("clangd")
+-- vim.lsp.config("clangd")
+
+-- NOTE: Lua LSP
+vim.lsp.enable("lua_ls")
+-- vim.lsp.config("lua_ls")
+
+-- NOTE: Markdown LSP
+vim.lsp.enable("marksman")
+-- vim.lsp.config("marksman")
