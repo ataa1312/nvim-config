3b4cf654246101af745cd541ef23175ff9188c4b
Ata Shaker <atashaker13@gmail.com>
2025-04-27 23:51:04 +0200

    NVIM: Add plugin: treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,74 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        auto_install = true,
+        sync_install = true,
+        ensure_installed = {
+            "bash",
+            "c",
+            "cpp",
+            "diff",
+            "html",
+            "javascript",
+            "jsdoc",
+            "json",
+            "jsonc",
+            "lua",
+            "luadoc",
+            "luap",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "regex",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "xml",
+            "yaml",
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<leader>sn",
+                node_incremental = "<leader>sn",
+                node_decremental = "<leader>sN",
+                scope_incremental = false,
+            },
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
+                    ["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
+                    ["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                    ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional region" },
+                    ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional region" },
+                    ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop region" },
+                    ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop region" },
+                    ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                },
+                selection_modes = {
+                    ['@parameter.outer'] = 'v', -- charwise
+                    ['@function.outer'] = 'V', -- linewise
+                    ['@class.outer'] = '<c-v>', -- blockwise
+                },
+                include_surrounding_whitespace = true,
+            },
+        },
+    },
+
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
+}
