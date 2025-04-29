15a06641e77432ed465ae1cb90a39047877373b3
Ata Shaker <atashaker13@gmail.com>
2025-04-30 01:30:39 +0200

    Reorganize LSP into Separate Files

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 vim.g.theme = "tokyonight-moon"
 require("config")
+require("lsp")
+  
+vim.lsp.config('*', {
+    capabilities = {
+        textDocument = {
+              semanticTokens = {
+                    multilineTokenSupport = true,
+            }
+        }
+    },
+    root_markers = { '.git' },
+})
+
+  
