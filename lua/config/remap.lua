5a5083a7a78b6031eff3d7c7e2072498bd66504e
Ata Shaker <atashaker13@gmail.com>
2025-05-06 00:12:35 +0200

    Add plugin: conform

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -6,11 +6,11 @@ vim.g.maplocalleader = "\\"
 
 -- NOTE: VIM Keymaps
 vim.keymap.set("n", "<leader>E", vim.cmd.Ex,
-                {desc="Open File Explorer"})
+    { desc = "Open File Explorer" })
 vim.keymap.set("n", "<leader>o", "o<Esc>",
-                {desc="Insert a New Line Below"})
+    { desc = "Insert a New Line Below" })
 vim.keymap.set("n", "<leader>O", "O<Esc>",
-                {desc="Insert a New Line Above"})
+    { desc = "Insert a New Line Above" })
 
 -- NOTE: Traversal Keymaps
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -19,47 +19,55 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- NOTE: Copy & Paste Links
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]],
-                {desc="Copy into The Clipboard"})
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]],
+    { desc = "Copy into The Clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]],
-                {desc="Copy into The Clipboard"})
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]],
-                {desc="Cut without Overwriting The Clipboard"})
+    { desc = "Copy into The Clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]],
+    { desc = "Cut without Overwriting The Clipboard" })
 
 vim.keymap.set("n", "<leader><leader>",
-               function() vim.cmd("so") end,
-                {desc="Source This File"})
+    function() vim.cmd("so") end,
+    { desc = "Source This File" })
 
 
 -- NOTE: FZF-Lua File/Tab Finder
-vim.keymap.set({"n", "v"}, "<leader>ff",
-  function() require("fzf-lua").files() end,
-  { desc = "FZF: Find Files" })
-vim.keymap.set({"n", "v"}, "<leader>ft",
-  function() require("fzf-lua").tabs() end,
-  { desc = "FZF: List Tabs" })
+vim.keymap.set({ "n", "v" }, "<leader>ff",
+    function() require("fzf-lua").files() end,
+    { desc = "FZF: Find Files" })
+vim.keymap.set({ "n", "v" }, "<leader>ft",
+    function() require("fzf-lua").tabs() end,
+    { desc = "FZF: List Tabs" })
 
--- NOTE:FZF-Lua Grep
-vim.keymap.set({"n", "v"}, "<leader>fgr",
-  function() require("fzf-lua").grep() end,
-  { desc = "FZF: Live Grep" })
-vim.keymap.set({"n", "v"}, "<leader>fgrl",
-  function() require("fzf-lua").grep_last() end,
-  { desc = "FZF: Resume Last Grep" })
+-- NOTE: FZF-Lua Grep
+vim.keymap.set({ "n", "v" }, "<leader>fgr",
+    function() require("fzf-lua").grep() end,
+    { desc = "FZF: Live Grep" })
+vim.keymap.set({ "n", "v" }, "<leader>fgrl",
+    function() require("fzf-lua").grep_last() end,
+    { desc = "FZF: Resume Last Grep" })
 
 -- NOTE: FZF-Lua Git
-vim.keymap.set({"n", "v"}, "<leader>fgf",
-  function() require("fzf-lua").git_files() end,
-  { desc = "FZF: Git Files" })
-vim.keymap.set({"n", "v"}, "<leader>fgs",
-  function() require("fzf-lua").git_status() end,
-  { desc = "FZF: Git Status" })
-vim.keymap.set({"n", "v"}, "<leader>fgc",
-  function() require("fzf-lua").git_commits() end,
-  { desc = "FZF: Git Commits" })
-vim.keymap.set({"n", "v"}, "<leader>fgbc",
-  function() require("fzf-lua").git_bcommits() end,
-  { desc = "FZF: Buffer Commits" })
-vim.keymap.set({"n", "v"}, "<leader>fgb",
-  function() require("fzf-lua").git_branches() end,
-  { desc = "FZF: Git Branches" })
+vim.keymap.set({ "n", "v" }, "<leader>fgf",
+    function() require("fzf-lua").git_files() end,
+    { desc = "FZF: Git Files" })
+vim.keymap.set({ "n", "v" }, "<leader>fgs",
+    function() require("fzf-lua").git_status() end,
+    { desc = "FZF: Git Status" })
+vim.keymap.set({ "n", "v" }, "<leader>fgc",
+    function() require("fzf-lua").git_commits() end,
+    { desc = "FZF: Git Commits" })
+vim.keymap.set({ "n", "v" }, "<leader>fgbc",
+    function() require("fzf-lua").git_bcommits() end,
+    { desc = "FZF: Buffer Commits" })
+vim.keymap.set({ "n", "v" }, "<leader>fgb",
+    function() require("fzf-lua").git_branches() end,
+    { desc = "FZF: Git Branches" })
+-- NOTE: Conform Keymaps
+vim.keymap.set({ "n", "v" }, "<leader>gw", function()
+    require("conform").format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+    })
+end, { desc = "Format File via Conform" })
