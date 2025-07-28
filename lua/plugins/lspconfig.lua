b6b1392a3bbdd3a9d75bd91e4266204249e6653d
Ata Shaker <atashaker13@gmail.com>
2025-07-28 21:30:55 +0200

    NVIM: Update lsp remaps giving priority to fzf-lua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,31 +2,51 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
         local lspconfig = require("lspconfig")
+        local util = require("lspconfig.util")
 
         local on_attach = function(client, bufnr)
             local bufmap = function(mode, lhs, rhs, desc)
+                if type(rhs) ~= "function" then
+                    vim.notify(string.format("LSP map %q -> %q is %s", lhs, desc, type(rhs)), vim.log.levels.ERROR)
+                    return
+                end
                 vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true, desc = desc })
             end
 
-            bufmap("n", "gd", vim.lsp.buf.definition, "Go to Definition")
-            bufmap("n", "gD", vim.lsp.buf.declaration, "Go to Declaration")
-            bufmap("n", "gi", vim.lsp.buf.implementation, "Go to Implementation")
-            bufmap("n", "gr", vim.lsp.buf.references, "Go to References")
             bufmap("n", "K", vim.lsp.buf.hover, "Hover Docs")
             bufmap("n", "<C-k>", vim.lsp.buf.signature_help, "Signature Help")
-            bufmap("n", "<leader>rn", vim.lsp.buf.rename, "Rename Symbol")
-            bufmap("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
-            -- bufmap("n", "<leader>f", function()
-            --     vim.lsp.buf.format({ async = true })
-            -- end, "Format Buffer")
 
             local fzf = require("fzf-lua")
+
             bufmap("n", "<leader>gd", fzf.lsp_definitions, "FZF: Go to Definition")
+            bufmap("n", "<leader>gD", fzf.lsp_declarations, "FZF: Go to Declaration")
+            bufmap("n", "<leader>gi", fzf.lsp_implementations, "FZF: Go to Implementation")
             bufmap("n", "<leader>gr", fzf.lsp_references, "FZF: Go to References")
+            if client.name == "pyright" then
+                -- bufmap("n", "<leader>K", fzf.lsp_hover, "FZF: Hover Docs")
+                -- bufmap("n", "<C-k>", fzf.lsp_signature_help, "FZF: Signature Help")
+                -- bufmap("n", "<leader>rn", fzf.lsp_rename, "FZF: Rename Symbol")
+                -- bufmap("n", "<leader>ca", fzf.lsp_range_code_actions, "FZF: Code Actions")
+            end
         end
 
-        lspconfig.pyright.setup({
+        lspconfig.basedpyright.setup({
+            root_dir = util.root_pattern({
+                "pyproject.toml",
+                ".git",
+            }),
             on_attach = on_attach,
+            settings = {
+                python = {
+                    analysis = {
+                        typeCheckingMode = "basic",
+                        autoSearchPaths = true,
+                        verboseOutput = true,
+                        -- useLibraryCodeForTypes = true,
+                    },
+                },
+            },
         })
+        lspconfig.texlab.setup({})
     end,
 }
