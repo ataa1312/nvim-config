a5200a41edbf76ac1625424afb05da4a080dee7e
Ata Shaker <atashaker13@gmail.com>
2025-08-18 00:51:40 +0200

    NVIM: Add remap: normal mode - <leader>X

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -7,6 +7,9 @@ vim.g.maplocalleader = "\\"
 -- NOTE: VIM Keymaps
 vim.keymap.set("n", "<leader>E", vim.cmd.Ex,
     { desc = "Open File Explorer" })
+vim.keymap.set("n", "<leader>X", vim.cmd.bd,
+    { desc = "Delete Current Buffer" })
+
 -- vim.keymap.set("n", "<leader>o", "o<Esc>",
 --     { desc = "Insert a New Line Below" })
 -- vim.keymap.set("n", "<leader>O", "O<Esc>",
