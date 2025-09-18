7e7ae0c42be4f8a46e52f8d4a966eecee6fb3546
Ata Shaker <atashaker13@gmail.com>
2025-09-18 22:41:27 +0200

    NVIM: Update nvim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,46 +1,45 @@
--- NOTE: Theme options 
+-- NOTE: Theme options
 vim.opt.termguicolors = true
 
--- NOTE: Numbering options 
+-- NOTE: Numbering options
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- NOTE: TAB options
-vim.opt.expandtab = true  -- NOTE: Pressing the TAB key will insert spaces instead of a TAB character
-vim.opt.tabstop = 4       -- NOTE: A TAB character looks like 4 spaces
-vim.opt.softtabstop = 4   -- NOTE: Number of spaces inserted instead of a TAB character
-vim.opt.shiftwidth = 4    -- NOTE: Number of spaces inserted when indenting
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
--- NOTE: Window options 
+-- NOTE: Window options
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 -- NOTE: Scrolling options
-vim.opt.scrolloff = 25
+vim.opt.scrolloff = 10
 
--- NOTE: Wrapping options 
+-- NOTE: Wrapping options
 vim.opt.wrap = true
-vim.opt.linebreak = true   -- only wrap at word boundaries
-vim.opt.breakindent = true -- indent wrapped lines
+vim.opt.linebreak = true
+vim.opt.breakindent = true
 vim.opt.colorcolumn = "80"
 vim.cmd("highlight ColorColumn guibg=#2e2e2e")
 
--- NOTE: Visual Mode options 
+-- NOTE: Visual Mode options
 vim.opt.virtualedit = "block"
 
--- NOTE: Search and Replace options 
+-- NOTE: Search and Replace options
 vim.opt.inccommand = "split"
 
-
--- NOTE: Folding options 
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- NOTE: Folding options
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevelstart = 99
 
 -- NOTE: Spellcheck options
 vim.opt.spell = true
 
--- NOTE: Visualbell options 
+-- NOTE: Visualbell options
 vim.opt.visualbell = true
 
 -- NOTE: Mouse options
@@ -51,4 +50,18 @@ vim.diagnostic.config({
     virtual_text = false,
     virtual_lines = true,
 })
+
+-- NOTE: Backup options
+local path = vim.fn.stdpath("config")
+local backup_path = path .. "/.backup"
+
+if not vim.fn.isdirectory(backup_path) then
+    vim.fn.mkdir(backup_path)
+end
+
+vim.o.backup = true
+vim.o.writebackup = true
+vim.o.backupdir = backup_path
+vim.o.backupext = ".bak"
+
 
