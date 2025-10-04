af1fe96e25b9db9ad0adbe8c3d0e949660794800
Ata Shaker <atashaker13@gmail.com>
2025-10-04 15:45:58 +0200

    NVIM: Move lsp config to lsp directory

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,3 @@
 vim.g.theme = "tokyonight-moon"
 require("config")
 require("lsp")
-  
-vim.lsp.config('*', {
-    capabilities = {
-        textDocument = {
-              semanticTokens = {
-                    multilineTokenSupport = true,
-            }
-        }
-    },
-    root_markers = { '.git' },
-})
-
-  
--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -3,3 +3,14 @@ require("lsp.basedpyright")
 require("lsp.clangd")
 require("lsp.marksman")
 require("lsp.tex-lab")
+
+vim.lsp.config("*", {
+    capabilities = {
+        textDocument = {
+            semanticTokens = {
+                multilineTokenSupport = true,
+            },
+        },
+    },
+    root_markers = { ".git" },
+})
