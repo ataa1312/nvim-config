00b58b90e4fb1b3e5375277a6dac5ad55f7c27be
Ata Shaker <atashaker13@gmail.com>
2026-01-08 17:33:58 +0100

    NVIM: Add texlab as an lsp to latex

--- nvim/lua/lsp/tex-lab.lua
+++ nvim/lua/lsp/tex-lab.lua
@@ -1,1 +1,12 @@
 vim.lsp.enable("texlab")
+vim.lsp.config["texlab"] = {
+    filetypes = { "tex" },
+    root_markers = { "main.tex" },
+    settings = {
+        texlab = {
+            build = {
+                useFileList = true,
+            },
+        },
+    },
+}
