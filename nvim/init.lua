3df9e8c90e1f74ea80d277f6a7cf9dbfacded267
Ata Shaker <atashaker13@gmail.com>
2026-04-18 13:13:24 +0200

    NVIM: Move general lsp config to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,13 @@
 vim.g.theme = "tokyonight-moon"
 require("config")
-require("lsp")
+
+vim.lsp.config("*", {
+    capabilities = {
+        textDocument = {
+            semanticTokens = {
+                multilineTokenSupport = true,
+            },
+        },
+    },
+    root_markers = { ".git" },
+})
