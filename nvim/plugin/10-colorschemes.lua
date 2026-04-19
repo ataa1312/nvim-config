6b5025b01c4ebecae48ec6543f4e1ad208866b69
Ata Shaker <atashaker13@gmail.com>
2026-04-19 16:39:01 +0200

    NVIM: Add colorscheme: catppuccin

--- nvim/plugin/10-colorschemes.lua
+++ nvim/plugin/10-colorschemes.lua
@@ -1,5 +1,6 @@
 vim.pack.add({
+    { src = utils.gh_url("catppuccin/nvim"), name = "catppuccin" },
     { src = utils.gh_url("folke/tokyonight.nvim"), name = "tokyonight" },
 })
 
-vim.cmd.colorscheme("tokyonight-moon")
+vim.cmd.colorscheme("catppuccin-mocha")
